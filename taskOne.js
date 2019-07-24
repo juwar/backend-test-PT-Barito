6f63d3edaf2e92b82e50a0f89bf098841df4195a
juwar <50343114+juwar@users.noreply.github.com>
2019-07-24 15:52:01 +0700

    Update taskOne.js

--- taskOne.js
+++ taskOne.js
@@ -1,6 +1,5 @@
 let library = ['pro', 'gram', 'merit', 'program', 'it', 'programmer']
 let input = 'programmerit'
-let change = input
 let getChart = 0
 let whitelist = []
 let output = ''
@@ -9,7 +8,7 @@ let junk = ''
 let validation = input.slice(0, 2)
 
 for (let i = 0; i < library.length; i++) {
-    getChart = change.search(library[i])
+    getChart = input.search(library[i])
     if (getChart >= 0) {
         whitelist.push(library[i])
     }
@@ -19,9 +18,9 @@ for (let l = 0; l < whitelist.length; l++) {
     junk = library[l]
     for (let i = 0; i < whitelist.length; i++) {
         temporary = junk + whitelist[i]
-        getChart = change.search(temporary)
+        getChart = input.search(temporary)
         if (junk.search(validation) >= 0) {
-            if (i == 0 && change.search(junk) >= 0) {
+            if (i == 0 && input.search(junk) >= 0) {
                 output += '\n' + junk
             }
             if (getChart >= 0) {
